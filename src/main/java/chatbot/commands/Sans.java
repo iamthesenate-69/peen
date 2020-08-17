425ee3c6f1f68aa47004f4e3119ecf64221ca14c
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-17 18:55:04 -0400

    2 second cooldown

--- src/main/java/chatbot/commands/Sans.java
+++ src/main/java/chatbot/commands/Sans.java
@@ -6,9 +6,10 @@ public class Sans extends Execute {
   
 	@Override
 	public void execute(MessageReceivedEvent e, String[] args) {
+		
 		String[] Title = {"Sans Undertale",
 				"Nothin' Personal Kid",
-				"Bad TIme",
+				"Bad Time",
 				"welp"};
 		String[] Url = {"https://upload.wikimedia.org/wikipedia/en/0/01/Sans_undertale.jpg",
 				"https://images.nintendolife.com/2e8fef0221f89/sans-undertale.original.jpg",
@@ -17,7 +18,7 @@ public class Sans extends Execute {
 				"https://thumbs.gfycat.com/EnchantedWelcomeBear-size_restricted.gif"};
 		
 		e.getChannel().sendMessage(Functions.build(Title, Url)).queue();
-		
+		Functions.setCooldown(e, 2000);
 	}
 
 	
