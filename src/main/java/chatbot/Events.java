9dde1f2624f19c0633336aabb3c0f9a9e6a85520
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-17 18:51:52 -0400

    added cooldown

--- src/main/java/chatbot/Events.java
+++ src/main/java/chatbot/Events.java
@@ -11,15 +11,25 @@ public class Events extends ListenerAdapter{
 
 		if (msg.getAuthor().isBot() || !msg.getContentRaw().startsWith(Main.prefix)) 
 			return;
-		
+
 		//Alright stop, this is too much, this is a complete clusterfuck, how did this get so out of control
 		//Andrej Karpathy: i literally have no idea 
 
 		String command = msg.getContentRaw().split(" ")[0].substring(Main.prefix.length());
 		
-		if (Main.commands.containsKey(command)) 
+		if (Main.commands.containsKey(command) && OnCooldown(msg.getMember()) <= 0) 
 			Main.commands.get(command).execute(event, msg.getContentRaw().split(" "));
-		
+
+
+
+	}
+
+	public int OnCooldown(Member m) {
+		for (int i = 0; i < Main.Bot.Timer.size(); i++) 
+			if (Main.Bot.Timer.get(i).hasMember(m))
+				return Main.Bot.Timer.get(i).t;
+
+		return 0;
 	}
 
 }
