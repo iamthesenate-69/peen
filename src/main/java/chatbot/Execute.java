1eaf2baa10eb7ed489d40904aab54d410f04162e
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-17 02:19:15 -0400

    Create Execute.java

---
+++ src/main/java/chatbot/Execute.java
@@ -0,0 +1,9 @@
+package chatbot;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+abstract class Execute {
+
+	public abstract void execute(MessageReceivedEvent e, String[] args);
+	public String Name;
+}
