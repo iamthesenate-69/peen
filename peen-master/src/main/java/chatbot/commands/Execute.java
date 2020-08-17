62bfb28c97753804a0e7e60a75072ca805c55e3a
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-17 14:17:26 -0400

    Delete Execute.java

--- peen-master/src/main/java/chatbot/commands/Execute.java
+++
@@ -1,8 +0,0 @@
-package chatbot.commands;
-
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
-public abstract class Execute {
-	public abstract void execute(MessageReceivedEvent e, String[] args);
-	
-}
