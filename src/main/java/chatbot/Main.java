ed15ef7638197ea75c1b3858de4170fce80d75a6
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-18 19:22:33 -0400

    optimized code

--- src/main/java/chatbot/Main.java
+++ src/main/java/chatbot/Main.java
@@ -1,6 +1,8 @@
 package chatbot;
 
-import net.dv8tion.jda.api.entities.Member;
+
+import net.dv8tion.jda.api.entities.User;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,34 +12,35 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import chatbot.commands.Ban;
-import chatbot.commands.CBT;
-import chatbot.commands.Execute;
-import chatbot.commands.Kick;
-import chatbot.commands.Megumin;
-import chatbot.commands.Sans;
-import java.util.Timer;
-import java.util.TimerTask; 
+import chatbot.commands.Command;
+import chatbot.commands.images.Megumin;
+import chatbot.commands.images.Sans;
+import chatbot.commands.images.ZeroTwo;
+import chatbot.commands.moderation.Ban;
+import chatbot.commands.moderation.Kick;
+import chatbot.commands.moderation.Remove;
+import chatbot.commands.notgoingtobeinthegithub.CBT;
+import chatbot.commands.notgoingtobeinthegithub.Crusade; 
 
 public class Main {
 
 	public static void main(String[] args) {
 		//token, activity, onlinestatus, bot prefix
 		try {
-			new Bot("Your Token Here", Activity.playing("activity"), OnlineStatus.DO_NOT_DISTURB, "~");
+			new Bot("Your Token here", Activity.watching("Activity"), OnlineStatus.DO_NOT_DISTURB, "~");
 		} catch(LoginException e) {
 			System.out.println("Provide a valid token!");
 			System.exit(1);
 		}
 	}
 
-	public static HashMap<String, Execute> commands;
+	public static HashMap<String, Command> commands;
 	public static String prefix;
 
 	public static class Bot {
 
 		//spam prevention
-		public static ArrayList<Limiter> Timer = new ArrayList<Limiter>();
+		public static ArrayList<Limiter> cooldown = new ArrayList<Limiter>();
 
 		Bot(String token, Activity activity, OnlineStatus status, String _prefix) throws LoginException{
 
@@ -46,61 +49,46 @@ public class Main {
 
 			prefix = _prefix;
 
-			commands = new HashMap<String, Execute>();
+			commands = new HashMap<String, Command>();
+			//games
+				//coming soon
+			//images
 			commands.put("sans", new Sans());
+			commands.put("megumin", new Megumin());
+			commands.put("zerotwo", new ZeroTwo());
+			//moderation
 			commands.put("ban", new Ban());
 			commands.put("kick", new Kick());
-			commands.put("megumin", new Megumin());
+			commands.put("remove", new Remove());
 
 			JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
 			.addEventListeners(new Events())
 			.setStatus(status)
 			.setActivity(activity)
 			.build();
-
-			new Timer().schedule(new Passtime(), 0, 100);
 		}
 
 
 
-	}
-
-	static class Passtime extends TimerTask {
-		@Override
-		public void run() {
-			for (int i = 0; i < Bot.Timer.size(); i++) {
-				int t = Bot.Timer.get(i).t;
-				t = t - 100;
-				if (t <= 0)
-					Bot.Timer.remove(i);
-				else 
-					Bot.Timer.get(i).updateTime(t);
-				
-			}
-			
-		}
 	}
 
 	public static class Limiter {
-		//member
+		//user
 		//time
-		Member member;
-		int t;
-		public Limiter (Member _member, int _t) {
-			member = _member;
+		User user;
+		long t;
+		public Limiter (User _user, long _t) {
+			user = _user;
 			t = _t;
 		}
 
-		public boolean hasMember(Member m) {
-			if (m.equals(member)) 
+		public boolean hasUser(User m) {
+			if (m.equals(user)) 
 				return true;
 
 			return false;
 		}
 		
-		public void updateTime(int n) {
-			t = n;
-		}
 
 	}
 
