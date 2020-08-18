9c7ad78a8ac708898a4678456eb9ed5a05f01b80
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2020-08-18 19:26:09 -0400

    cleaning

--- src/main/java/chatbot/commands/Ban.java
+++ src/main/java/chatbot/commands/Ban.java
@@ -1,52 +1,38 @@
-package chatbot.commands;
+package chatbot.commands.moderation;
 
-
-import java.util.List;
+import chatbot.commands.Command;
+import chatbot.commands.Functions;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class Ban extends Execute {
+public class Ban extends Command {
 
 	@Override
 	public void execute(MessageReceivedEvent e, String[] args) {
 
-		List<Member> members = e.getMessage().getMentionedMembers();
-		
-		//if user is not a mod
-		if (!e.getMember().getPermissions().contains(Permission.BAN_MEMBERS)) {
-			e.getChannel().sendMessage("You do not have BAN_MEMBERS permission").queue();
+		if (Functions.checkPerms(e, Permission.BAN_MEMBERS))
 			return;
-		}
-		
-		//if bot has no perms
-		else if (!e.getGuild().getSelfMember().getPermissions().contains(Permission.BAN_MEMBERS)) {
-			e.getChannel().sendMessage("I do not have BAN_MEMBERS permission").queue();
-			return;
-		}
-		
-		//if message did not mention
-		else if (members.size() == 0) {
+
+
+		if (e.getMessage().getMentionedMembers().size() == 0) {
 			e.getChannel().sendMessage("You need to mention a member").queue();
 			return;
 		}
-		
-		String message = "";
+
+		Member member = e.getMessage().getMentionedMembers().get(0);
 
 		//gets the ban message
-		for (int i = 2; i < args.length; i++) {
+		String message = "";
+		for (int i = 2; i < args.length; i++) 
 			message = message + " " + args[i];
-		}
+		
 
-		//if message is unprovided
-		if (message == "") message = "banned by " + e.getMember().getEffectiveName();
-
-	
-			e.getGuild().ban(members.get(0), 0, message).queue(success -> {	//catch errors
-				e.getChannel().sendMessage("Banned " + members.get(0).getEffectiveName()).queue();
-			}, error ->{
-				e.getChannel().sendMessage("Unable to ban " + members.get(0).getEffectiveName()).queue();
-			});
+		e.getGuild().ban(member, 0, message).queue(success -> {	//catch errors
+			e.getChannel().sendMessage("Banned " + member.getEffectiveName()).queue();
+		}, error ->{
+			e.getChannel().sendMessage("Unable to ban " + member.getEffectiveName()).queue();
+		});
 
 
 	}
